ef0532b6d18b4e434c5ee61ff9626c8adcfac893
laireyx <laireyx@gmail.com>
2023-01-12 22:51:20 +0900

    Test commit

---
+++ src/main/kotlin/SimpleConsumer.kt
@@ -0,0 +1,34 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.util.*
+
+class SimpleConsumer {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    private val TOPIC_NAME = "test"
+    private val GROUP_ID = "test-group"
+
+    fun testConsumer() {
+        val configs = javaClass.classLoader.getResourceAsStream("kafka.properties").use {
+            Properties().apply { load(it) }
+        }
+        configs[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
+        configs[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        configs[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+
+        val consumer = KafkaConsumer<String, String>(configs)
+        consumer.subscribe(listOf(TOPIC_NAME))
+
+        while (true) {
+            val records = consumer.poll(Duration.ofSeconds(1))
+
+            records.forEach { record ->
+                logger.info("$record")
+            }
+        }
+    }
+}
