2350cea21c7b8fd115fc321a4fab394d3e8b2aa1
laireyx <laireyx@gmail.com>
2023-01-12 21:17:40 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation(kotlin("reflect"))
+
+    implementation("org.apache.kafka:kafka-clients:3.3.1")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
