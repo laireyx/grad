5b6d70667c6fa8bc58ab6ec2c0af709eed6e4803
laireyx <laireyx@gmail.com>
2023-02-07 16:10:17 +0900

    Test Platform으로 테스트 코드 이동

--- src/main/kotlin/TestPlatform.kt
+++ src/main/kotlin/TestPlatform.kt
@@ -1,22 +1,28 @@
-class TestPlatform(
-    private val producerNum: Int,
-    private val consumerNum: Int,
-    private val topicNum: Int,
-    private val partitionNum: Int,
-    private val produceSpeedMs: Int,
-    private val consumeSpeedMs: Int,
-    private val messageLengthMin: Int,
-    private val messageLengthMax: Int,
-) {
-    private val consumers: Array<SimpleConsumer> = Array(consumerNum) { SimpleConsumer() }
-    private val producers: Array<SimpleProducer> = Array(producerNum) { SimpleProducer(it) }
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.*
+
+class TestPlatform() {
+    private val configs = javaClass.classLoader.getResourceAsStream("test.properties").use {
+        Properties().apply { load(it) }
+    }
+
+    private val topics: Array<String> = Array(configs["topic.size"].toString().toInt()) { "topic-$it" }
+    private val partitions: Array<String> = Array(configs["partition.size"].toString().toInt()) { "partition-$it" }
+
+    private val consumers: Array<SimpleConsumer> = Array(configs["consumer.size"].toString().toInt()) { SimpleConsumer() }
+    private val producers: Array<SimpleProducer> = Array(configs["producer.size"].toString().toInt()) { SimpleProducer(it) }
 
     init {
         // TODO: initialize something
-
     }
-    public fun test() {
-        // TODO: test something
 
+    suspend fun test() = runBlocking {
+        consumers.forEach {
+            launch { it.testConsumer() }
+        }
+        producers.forEach {
+            launch { it.testSimpleProducer() }
+        }
     }
 }
