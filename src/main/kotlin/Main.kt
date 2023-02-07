b51a6cea7e2ae4df92e6f6ed5a4daa719f036328
laireyx <laireyx@gmail.com>
2023-02-07 16:10:17 +0900

    Test Platform으로 테스트 코드 이동

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 suspend fun main() = runBlocking {
-    launch { SimpleConsumer().testConsumer() }
-    repeat(5) {
-        launch { SimpleProducer(it).testSimpleProducer() }
-    }
+    TestPlatform().test()
 }
